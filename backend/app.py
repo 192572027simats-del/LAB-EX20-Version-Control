80501960d6db66bc96a132e4a17c56e473c66ac0
Ganesh Kumar <ganesh8667291198@gmail..com>
2026-08-22 11:14:46 +0530

    Develop backend module

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,6 @@
-print("Backend Module")
-print("GitHub Version Control Project")
+def start_application():
+    print("Backend application started")
+    print("LAB EX20 Version Control Project")
+
+
+start_application()
